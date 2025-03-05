0d25c43a4a6004a587d2adc2af6b4cd55c1d2bd0
ruslanboleev <ruslanboleev007@gmail.com>
2025-03-05 10:57:02 +0700

    TaskOne

--- Java521BoleevRecursion/src/TaskOne.java
+++ Java521BoleevRecursion/src/TaskOne.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class TaskOne {
@@ -9,27 +8,39 @@ public class TaskOne {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите число: ");
-        int input = scanner.nextInt();
-        int[] array = new int[input];
+        int limit = scanner.nextInt();
+        int[] array = new int[limit];
 
-        IntStream.range(0, input)
+        IntStream.range(0, limit)
                 .forEach(index -> array[index] = index);
 
-        derivationFibonacci(array);
 
-        System.out.print(Arrays.toString(array) +
-                " count: " + array.length);
+        // Список для хранения чисел Фибоначчи
+        List<Integer> filteredFibonacci = new ArrayList<>();
+
+        // Заполняем массив и фильтруем числа
+        derivationFibonacci(array,limit,filteredFibonacci);
+
+        System.out.print("Числа Фибоначчи до " + array.length + ": " + filteredFibonacci);
     }
 
-    public static void derivationFibonacci(int[] arr) {
+    public static void derivationFibonacci(int[] arr,int limit,List<Integer> filteredFibonacci) {
         if (arr.length >= 1) {
             arr[0] = 0;
+            filteredFibonacci.add(arr[0]);
         }
         if (arr.length >= 2) {
             arr[1] = 1;
+            filteredFibonacci.add(arr[1] );
             }
+
+        // Заполняем массив и фильтруем числа
         for (int i = 2; i < arr.length; i++) {
             arr[i] = arr[i - 2] + arr[i - 1];
+            if (arr[i] > limit) {
+                break;
+            }
+            filteredFibonacci.add(arr[i]);
         }
     }
 }
